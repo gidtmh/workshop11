7bc3c075fcc625f803b7407c6c68a7c2c04478f7
gidtmh <gideontmh@gmail.com>
2022-01-05 15:06:17 +0800

    new

---
+++ src/main/java/sg/edu/nus/ibfssfworkshop11/Ibfssfworkshop11Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.ibfssfworkshop11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ibfssfworkshop11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ibfssfworkshop11Application.class, args);
+	}
+
+}
